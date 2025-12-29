3249351c92c1053a56b0861f0e3d7a8a07ee1949
Shishir Jessu <shishir.jessu@gmail.com>
2025-12-29 14:56:38 -0600

    Move breakpoint tests to BreakpointTest file

--- test/BreakpointTest.cpp
+++ test/BreakpointTest.cpp
@@ -8,6 +8,83 @@
 #include <process.hpp>
 
 namespace sdb::test {
+    TEST(ProcessTest, CreateBreakpointSites) {
+        auto myProc = Process::launch("test/targets/run_forever");
+
+        const auto& myBreakpointSites = myProc->getBreakpointSites();
+        EXPECT_EQ(myBreakpointSites.size(), 0);
+        EXPECT_TRUE(myBreakpointSites.empty());
+
+        auto myBreakpointSiteCheck = [&](const BreakpointSite& mySite,
+                                         VirtualAddress anAddress,
+                                         BreakpointSiteId anId) {
+            EXPECT_EQ(mySite.getId(), anId);
+            EXPECT_EQ(mySite.getAddress(), anAddress);
+
+            EXPECT_TRUE(myBreakpointSites.contains_id(anId));
+            EXPECT_TRUE(myBreakpointSites.contains_address(anAddress));
+
+            EXPECT_EQ(myBreakpointSites.getById(anId).getId(), anId);
+            EXPECT_EQ(myBreakpointSites.getByAddress(anAddress).getAddress(),
+                      anAddress);
+        };
+
+        BreakpointSite& myFirstSite =
+            myProc->createBreakpointSite(VirtualAddress{0});
+        EXPECT_EQ(myBreakpointSites.size(), 1);
+        EXPECT_FALSE(myBreakpointSites.empty());
+
+        EXPECT_EQ(myFirstSite.getAddress(), VirtualAddress{0});
+
+        for (std::uint64_t i = 1; i < 10; ++i) {
+            BreakpointSite& myCurSite =
+                myProc->createBreakpointSite(VirtualAddress{i});
+
+            myBreakpointSiteCheck(myCurSite, VirtualAddress{i},
+                                  myFirstSite.getId() + i);
+
+            EXPECT_EQ(myBreakpointSites.size(), i + 1);
+            EXPECT_FALSE(myBreakpointSites.empty());
+        }
+    }
+
+    TEST(ProcessTest, MissingBreakpointSites) {
+        auto myProc = Process::launch("test/targets/run_forever");
+        const auto& cproc = myProc;
+
+        EXPECT_THROW(
+            myProc->getBreakpointSites().getByAddress(VirtualAddress{44}),
+            sdb::Error);
+        EXPECT_THROW(myProc->getBreakpointSites().getById(BreakpointSiteId{44}),
+                     sdb::Error);
+        EXPECT_THROW(
+            cproc->getBreakpointSites().getByAddress(VirtualAddress{44}),
+            sdb::Error);
+        EXPECT_THROW(cproc->getBreakpointSites().getById(BreakpointSiteId{44}),
+                     sdb::Error);
+    }
+
+    TEST(ProcessTest, IteratingOverBreakpoints) {
+        auto myProc = Process::launch("test/targets/run_forever");
+        const auto& cproc = myProc;
+
+        VirtualAddress myStartingAddr{42};
+
+        myProc->createBreakpointSite(myStartingAddr);
+        myProc->createBreakpointSite(myStartingAddr + 1);
+        myProc->createBreakpointSite(myStartingAddr + 2);
+        myProc->createBreakpointSite(myStartingAddr + 3);
+
+        myProc->getBreakpointSites().forEach(
+            [myStartingAddr](auto& mySite) mutable {
+                EXPECT_EQ(mySite.getAddress(), myStartingAddr++);
+            });
+
+        cproc->getBreakpointSites().forEach(
+            [myStartingAddr](auto& mySite) mutable {
+                EXPECT_EQ(mySite.getAddress(), myStartingAddr++);
+            });
+    }
 
     TEST(BreakpointSetTest, SetBreakpoint) {
         Pipe myPipe(false);
